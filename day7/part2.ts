f1f01e7a980bd8a648b85dd1408aa33c87bca365
Brandon Freeman <freemanb2@hotmail.com>
2025-12-08 18:36:12 -0500

    Day 7 part 2

--- day7/part2.ts
+++ day7/part2.ts
@@ -1,47 +1,35 @@
 import * as fs from "fs";
 import * as path from "path";
-import * as _ from "lodash";
 
 const inputFile = path.join(__dirname, "input.txt");
 const inputText = fs.readFileSync(inputFile, "utf8");
 const inputs = inputText.split(/\r?\n/);
 
+function calculateTimeline(depth: number, beam: number): number {
+    if (depth >= inputs.length - 1) {
+        return 1;
+    }
+
+    if (memo[depth][beam] > -1) {
+        return memo[depth][beam];
+    }
+
+    let count = 0;
+    if (inputs[depth].charAt(beam) === '^') {
+        count += calculateTimeline(depth + 1, beam - 1);
+        count += calculateTimeline(depth + 1, beam + 1);
+    } else {
+        count += calculateTimeline(depth + 1, beam);
+    }
+
+    memo[depth][beam] = count;
+    return count;
+}
+
+let memo = Array.from({ length: inputs.length }, () =>
+    Array.from({ length: inputs[0].length }, () => -1)
+);
 const entrance = inputs[0].indexOf('S');
-
-function getNextSplit(remainingPath: Array<string>, beamPosition: number): number {
-    for (let i = 0; i < remainingPath.length; i++) {
-        if (remainingPath[i].charAt(beamPosition) === '^') return i;
-    }
-    return -1;
-}
-
-let possibleTimelines = 1;
-function parseTimeline(beamPositions: Array<boolean>, remainingPath: Array<string>, beamPosition: number) {
-    beamPositions[beamPosition] = false;
-    if (!beamPositions[beamPosition - 1]) {
-        const nextSplit = getNextSplit(remainingPath, beamPosition - 1);
-        if (nextSplit > -1) {
-            const leftBeamPositions = beamPositions.slice();
-            leftBeamPositions[beamPosition - 1] = true;
-            parseTimeline(leftBeamPositions, remainingPath.slice(nextSplit), beamPosition - 1);
-        }
-    }
-
-    if (!beamPositions[beamPosition + 1]) {
-        possibleTimelines++;
-        if (possibleTimelines % 10000000 === 0) console.log(possibleTimelines);
-        const nextSplit = getNextSplit(remainingPath, beamPosition + 1);
-        if (nextSplit > -1) {
-            const rightBeamPositions = beamPositions.slice();
-            rightBeamPositions[beamPosition + 1] = true;
-            parseTimeline(rightBeamPositions, remainingPath.slice(nextSplit), beamPosition + 1);
-        }
-    }
-}
-
-const initialBeamPositions = new Array<boolean>(inputs[0].length).fill(false);
-initialBeamPositions[entrance] = true;
-const nextSplit = getNextSplit(inputs, entrance);
-parseTimeline(initialBeamPositions.slice(), inputs.slice(nextSplit), entrance);
+const possibleTimelines = calculateTimeline(1, entrance);
 
 console.log(possibleTimelines);
